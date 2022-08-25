a536e12a5ed5f8fd0c27b21702908de7fbcd2339
San <andreal87@mail.ru>
2022-08-25 11:34:11 +0600

    bug fix

--- utils.py
+++ utils.py
@@ -3,28 +3,24 @@ from typing import Iterable
 from exceptions import NotBoolConvertedType, NotIntConvertedType
 
 
-def build_query(it, cmd: str, value: str) -> Iterable:
+def build_query(it: Iterable, cmd: str, value: str) -> Iterable:
     res = list(map(lambda v: v.strip(), it))
     if cmd == 'filter':
-        res = filter(lambda v: value in v, res)
+        res = list(filter(lambda v: value in v, res))
     elif cmd == 'sort':
-        if value == "True":
-            value = True
-        elif value == "False":
-            value = False
-        else:
+        if value not in ["True", "False"]:
             raise NotBoolConvertedType
-        res = sorted(res, reverse=value)
+        res = sorted(res, reverse=value == "True")
     elif cmd == 'unique':
-        res = set(res)
+        res = list(set(res))
     elif cmd == 'regex':
         regex = re.compile(value)
-        res = filter(lambda v: regex.search(v), res)
+        res = list(filter(lambda v: regex.search(v), res))
     try:
         if cmd == 'limit':
             res = list(res[:int(value)])
         if cmd == 'map':
-            res = map(lambda v: v.split(' ')[int(value)], res)
+            res = list(map(lambda v: v.split(' ')[int(value)], res))
     except ValueError:
         raise NotIntConvertedType
     return res
